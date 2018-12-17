cd64dfe1b9ee5f6689fafe8d5dae6db90fd7b35a
Equallax <34425132+Equallax@users.noreply.github.com>
2018-12-17 19:45:31 +0100

    updates

--- script/script.js
+++ script/script.js
@@ -1,1 +1,8 @@
-test bla bla
+const woop = document.getElementById("test");
+
+
+window.addEventListener("scroll", function()
+    {
+    let offset = window.pageYOffset;
+    woop.style.backgroundPositionY = offset / 10 + "px";
+    })
